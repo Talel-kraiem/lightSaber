88012a5fe347815fa83e2982ff5f9b161721f6a5
Talel Kraiem <kraiemtalel4@gmail.com>
2023-06-20 16:46:42 +0100

    first working graph

---
+++ testing/index.js
@@ -0,0 +1,9 @@
+/*
+Author: Talel Kraiem (kraiemtalel4@gmail.com)
+index.js (c) 2023
+Desc: description
+Created:  2023-06-18T20:12:48.396Z
+Modified: !date!
+*/
+import { $ } from "../lib/dep/functionDefenition.js";
+console.log($("#lightsaber"));
